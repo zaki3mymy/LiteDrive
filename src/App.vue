c77240b36e0f2d756e60f240db8f146df83425c9
zaki3mymy <zaki.last@gmail.com>
2023-08-11 15:50:04 +0900

    画面の構成を変更

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,24 @@ import { Authenticator } from '@aws-amplify/ui-vue'
 </script>
 
 <template>
-  <authenticator>
   <!-- <authenticator hide-sign-up="true"> -->
+  <authenticator>
     <template v-slot="{ signOut }">
-      <header>
-        <h1>MyCloudStorage</h1>
-        <button @click="signOut" class="contact">Sign out</button>
-      </header>
-      <MainView></MainView>
+      <v-layout class="rounded rounded-md">
+        <v-app-bar title="owncloudbox">
+          <v-btn variant="tonal" @click="signOut">Sign out</v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer>
+          <v-list>
+            <v-list-item title="draft"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+          <MainView></MainView>
+        </v-main>
+      </v-layout>
     </template>
   </authenticator>
 </template>
