ecdb2fb319f5ada8c7738f3a74596865a2c857a9
zaki3mymy <zaki.last@gmail.com>
2023-08-11 15:16:07 +0900

    amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s339cf5d0d": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
