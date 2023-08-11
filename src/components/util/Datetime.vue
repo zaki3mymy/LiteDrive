4e5829c0bb4cd0473b03bb83e8327d8de24139ed
zaki3mymy <zaki.last@gmail.com>
2023-08-11 15:28:38 +0900

    momentで日時操作を行うコンポーネントを追加

---
+++ src/components/util/Datetime.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import moment from "moment";
+
+const formatDatetime = (date: Date) => {
+  const dt = moment(date);
+  return dt.format("YYYY-MM-DD hh:mm:ss");
+};
+
+export { formatDatetime };
+</script>
