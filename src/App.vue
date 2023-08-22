9e1fb084e4bf1923d665165b531e5a5e6047dd74
zaki3mymy <zaki.last@gmail.com>
2023-08-22 17:37:47 +0900

    ヘッダーを追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MainView from './views/MainView.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 import { Authenticator } from '@aws-amplify/ui-vue'
 </script>
 
@@ -7,10 +8,8 @@ import { Authenticator } from '@aws-amplify/ui-vue'
   <!-- <authenticator hide-sign-up="true"> -->
   <authenticator>
     <template v-slot="{ signOut }">
-      <button @click="signOut">Sign out</button>
-      <main>
-        <MainView></MainView>
-      </main>
+      <HeaderComponent :signOut="signOut"></HeaderComponent>
+      <MainView></MainView>
     </template>
   </authenticator>
 </template>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="grey darken-4">
+    <div class="nav-wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col s12">
+            <a href="/" class="brand-logo">OwnCloudBox</a>
+            <div class="right">
+              <button class="waves-effect waves-light grey btn hide-on-small-only" @click="signOut">Sign out</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    signOut: null
+  }
+}
+</script>
