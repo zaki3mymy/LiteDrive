1df41b061371044740503dfbbd13229a17c4a368
zaki3mymy <zaki.last@gmail.com>
2023-09-01 23:10:34 +0900

    色を変更

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav class="grey darken-4">
+  <nav class="blue-grey darken-4">
     <div class="nav-wrapper">
       <div class="container">
         <div class="row">
           <div class="col s12">
             <a href="/" class="brand-logo">serverless box</a>
             <div class="right">
-              <button class="waves-effect waves-light grey btn hide-on-small-only" @click="signOut">Sign out</button>
+              <button class="waves-effect waves-light blue-grey btn hide-on-small-only" @click="signOut">Sign out</button>
             </div>
           </div>
         </div>
