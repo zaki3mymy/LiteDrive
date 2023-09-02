63af043582ec9af31556762749c90773c1d66d29
zaki3mymy <zaki.last@gmail.com>
2023-09-02 15:09:33 +0900

    日時のフォーマットを変更

--- src/components/util/Datetime.vue
+++ src/components/util/Datetime.vue
@@ -3,8 +3,13 @@ import moment from "moment";
 
 const formatDatetime = (date: Date) => {
   const dt = moment(date);
-  return dt.format("YYYY-MM-DD hh:mm:ss");
+  return dt.format("YYYY/MM/DD hh:mm:ss");
 };
 
-export { formatDatetime };
+const shortFormatDatetime = (date: Date) => {
+  const dt = moment(date);
+  return dt.format("MM/DD");
+};
+
+export { formatDatetime, shortFormatDatetime };
 </script>
