9d687d4b7ad9e3fbc202f7ced36b33ddecdf486f
zaki3mymy <zaki.last@gmail.com>
2023-09-03 14:13:36 +0900

    一覧にnameだけ表示するように変更

--- src/components/ListObjects.vue
+++ src/components/ListObjects.vue
@@ -19,20 +19,20 @@ import MoreOperation from './MoreOperation.vue'
       </tr>
     </thead>
     <tbody>
-      <tr v-for="folder in folders" :key="folder">
+      <tr v-for="folder in folders" :key="folder.key">
         <td><i class="material-icons">folder</i></td>
         <td>
-          <router-link :to="'/' + folder">{{ folder }}</router-link>
+          <router-link :to="'/' + folder.key">{{ folder.name }}</router-link>
         </td>
         <td></td>
         <td>
-          <MoreOperation :objectKey="folder"></MoreOperation>
+          <MoreOperation :objectKey="folder.key"></MoreOperation>
         </td>
       </tr>
       <tr v-for="file in files" :key="file.key">
         <td></td>
         <td>
-          <router-link :to="file.key">{{ file.key }}</router-link>
+          <router-link :to="file.key">{{ file.name }}</router-link>
         </td>
         <td>
           <span class="hide-on-small-only">
@@ -57,6 +57,23 @@ import MoreOperation from './MoreOperation.vue'
 <script>
 import M from 'materialize-css'
 
+class S3Object {
+  constructor(key, lastModified) {
+    this.key = key
+    this.lastModified = lastModified
+
+    let name = ""
+    if (key.endsWith("/")) {
+      // フォルダ
+      name = key.split("/").slice(-2)[0] + "/"
+    } else {
+      // ファイル
+      name = key.split("/").slice(-1)[0]
+    }
+    this.name = name
+  }
+}
+
 export default {
   props: {
     keyProp: String
@@ -68,14 +85,14 @@ export default {
   data() {
     return {
       files: [],
-      folders: new Set(),
+      folders: [],
       isFetching: true
     }
   },
   methods: {
     async listObjects(key) {
       this.files = []
-      this.folders = new Set()
+      this.folders = []
 
       // 現在の階層を取得("/"の数を階層とする)
       const level = (key.match(/\//g) || []).length
@@ -89,22 +106,36 @@ export default {
       const response = await Storage.list(key, config)
       this.isFetching = false
 
+      const files = []
+      const folders = []
+      const folderKeys = new Set()
       response.results.forEach((res) => {
         // 階層が同じものだけを対象とする(フォルダーは+1階層)
         const resLevel = (res.key.match(/\//g) || []).length
 
+        const obj = new S3Object(res.key, res.lastModified)
+
         if (res.size) {
-          if (level == resLevel) this.files.push(res)
-          // sometimes files declare a folder with a / within then
-          let possibleFolder = res.key.split('/').slice(0, -1).join('/')
-          if (possibleFolder) {
-            possibleFolder += '/'
-            if (level + 1 == resLevel) this.folders.add(possibleFolder)
+          // ファイル
+          if (level == resLevel) files.push(obj)
+
+          // ファイルまでの階層をフォルダとして追加
+          const parentFolder = res.key.split("/").slice(0, -1).join("/") + "/"
+          if (parentFolder !== "/" && level + 1 == resLevel && !folderKeys.has(parentFolder)) {
+              const folderObj = new S3Object(parentFolder, null)
+              folders.push(folderObj)
+              folderKeys.add(parentFolder)
           }
         } else {
-          if (level + 1 == resLevel) this.folders.add(res.key)
+          // フォルダ
+          if (level + 1 == resLevel && !folderKeys.has(obj.key)) {
+            folders.push(obj)
+            folderKeys.add(obj.key)
+          }
         }
       })
+      this.files = files
+      this.folders = folders
     }
   },
   watch: {
