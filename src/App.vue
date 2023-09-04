12d8c191c8e1e32a096b2453a3cf5734ed745d80
zaki3mymy <zaki.last@gmail.com>
2023-09-04 19:44:38 +0900

    サインアップを外す

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import { Authenticator } from '@aws-amplify/ui-vue'
 </script>
 
 <template>
-  <!-- <authenticator hide-sign-up="true"> -->
-  <authenticator>
+  <authenticator hide-sign-up="true">
+  <!-- <authenticator> -->
     <template v-slot="{ signOut }">
       <HeaderComponent :signOut="signOut"></HeaderComponent>
       <MainView></MainView>
