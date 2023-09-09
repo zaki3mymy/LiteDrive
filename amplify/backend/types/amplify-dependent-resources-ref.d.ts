e3bdde3d6f2b1ff650b64fb4f1446986c5ff515a
zaki3mymy <zaki.last@gmail.com>
2023-09-10 02:17:58 +0900

    chore: create backend
    - amplify init
    - amplify add auth
    - amplify add storage
    - amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "litedrive8154d63f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3505b56d3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
